ed0411f83c2064b3b2eff80930938c1050fea019
N&G Interprises <neginterprises@gmail.com>
2025-04-22 15:03:29 -0300

    Finalizando javascript com parte calcular

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ function back() {
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
 }
+
+function calcular() {
+    // Parte de calcular
+    var resultado = document.getElementById('resultado').innerHTML;
+    if (resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    } else {
+        document.getElementById('resultado').innerHTML = "0";
+    }
+}
