f7341ed223289d9810e28d3f8493e2e3cd4b05a8
N&G Interprises <neginterprises@gmail.com>
2025-04-22 14:57:29 -0300

    Botão de apagar no javascript

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ function clean() {
     // Parte de limpeza
     document.getElementById('resultado').innerHTML = "";
 }
+
+function back() {
+    // Parte de apagar
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
+}
